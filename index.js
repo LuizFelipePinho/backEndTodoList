9bbea51b2d556e8b3bfbacd31a1b9bb0d0cc7257
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-10-18 22:28:41 -0300

    adicionando variavel de ambiente

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const Connection = require("./connection/conn.js");
 app.use(express.json());
 app.use(cors());
-Connection();
+
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+
+Connection(db_url, db_user, db_pass, db_data);
 
 const rotaToDoList = require("./routes/rotas.js");
 
